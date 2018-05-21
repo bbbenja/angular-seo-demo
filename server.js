6111d5359dada08a1f3bc6180f272f85c4d47ebd
Benjamin COUSIN <benou.cousin@gmail.com>
2018-05-21 15:19:42 +0200

    chore(*): add google verification file + morgan express logger

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const rendertron = require('rendertron-middleware');
-var fs = require('fs');
+const fs = require('fs');
 const crawlers = fs.readFileSync('crawler-user-agents.json');
-
+const morgan = require('morgan');
 const app = express();
 
 function getCrawlersPattern() {
-  return new RegExp(crawlers.map(c => c.pattern).join('|'), 'i');
+  return new RegExp(JSON.parse(crawlers).map(c => c.pattern).join('|'), 'i');
 }
 
 app.use(rendertron.makeMiddleware({
@@ -15,7 +15,12 @@ app.use(rendertron.makeMiddleware({
 }));
 
 app.use(express.static(__dirname + '/dist/demo-seo'));
+app.use(morgan('combined'));
 app.listen(8081);
+
+app.get('google1dbef6dede4b34f9.html', function(req, res){
+  res.sendFile(__dirname + '/dist/demo-seo/google1dbef6dede4b34f9.html');
+});
 
 app.get('*', function(req, res){
   res.sendFile(__dirname + '/dist/demo-seo/index.html');
